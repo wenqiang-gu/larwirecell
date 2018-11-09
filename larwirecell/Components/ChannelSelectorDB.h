8f40fc2a8496d171c8c2dae28efb9cfa0f929588
Hanyu Wei <hanyuwei@uboonegpvm07.fnal.gov>
2018-11-08 20:31:13 -0600

    dynamic misconfigured channels for simulation overlay

---
+++ larwirecell/Components/ChannelSelectorDB.h
@@ -0,0 +1,46 @@
+#ifndef LARWIRECELL_COMPONENTS_CHANNELSELECTORDB
+#define LARWIRECELL_COMPONENTS_CHANNELSELECTORDB
+
+#include "larwirecell/Components/ChannelNoiseDB.h"
+#include "WireCellIface/IChannelNoiseDatabase.h"
+
+namespace wcls {
+
+    class ChannelSelectorDB : public ChannelNoiseDB
+    {
+    public:
+
+	ChannelSelectorDB();
+	virtual ~ChannelSelectorDB();
+
+        /// IArtEventVisitor.
+	// Note: we don't actually poke at the event but use this
+	// entry to refresh info from services in case they change
+	// event-to-event.
+        virtual void visit(art::Event & event);
+	
+        /// IConfigurable.
+        virtual void configure(const WireCell::Configuration& config);
+
+        virtual channel_group_t bad_channels() const {
+        return m_bad_channels;
+        }
+        virtual channel_group_t miscfg_channels() const {
+        return m_miscfg_channels;
+        }
+
+    private:
+        std::string m_type;
+        channel_group_t m_bad_channels;
+        channel_group_t m_miscfg_channels;
+    };
+    
+
+}  // wcls
+
+#endif /* LARWIRECELL_COMPONENTS_CHANNELSELECTORDB */
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// End:
