777bea7f51fca03c7705a49ffae0dc7be4d45432
Brett Viren <brett.viren@gmail.com>
2018-11-19 16:43:37 -0500

    Add a WCT source which converts from recob::Wire to IFrame.
    Compiles but still needs real testing.

---
+++ larwirecell/Components/CookedFrameSource.cxx
@@ -0,0 +1,142 @@
+#include "CookedFrameSource.h"
+#include "art/Framework/Principal/Handle.h" 
+
+#include "lardataobj/RecoBase/Wire.h"
+#include "art/Framework/Principal/Event.h"
+
+#include "TTimeStamp.h"
+
+
+#include "WireCellIface/SimpleFrame.h"
+#include "WireCellIface/SimpleTrace.h"
+#include "WireCellUtil/NamedFactory.h"
+
+WIRECELL_FACTORY(wclsCookedFrameSource, wcls::CookedFrameSource,
+		 wcls::IArtEventVisitor, WireCell::IFrameSource)
+
+
+using namespace wcls;
+using namespace WireCell;
+
+CookedFrameSource::CookedFrameSource()
+    : m_nticks(0)
+{
+}
+
+CookedFrameSource::~CookedFrameSource()
+{
+}
+
+
+WireCell::Configuration CookedFrameSource::default_configuration() const
+{
+    Configuration cfg;
+    cfg["art_tag"] = "";        // how to look up the cooked digits
+    cfg["tick"] = 0.5*WireCell::units::us;
+    cfg["frame_tags"][0] = "orig"; // the tags to apply to this frame
+    cfg["nticks"] = m_nticks; // if nonzero, truncate or zero-pad frame to this number of ticks.
+    return cfg;
+}
+
+void CookedFrameSource::configure(const WireCell::Configuration& cfg)
+{
+    const std::string art_tag = cfg["art_tag"].asString();
+    if (art_tag.empty()) {
+        THROW(ValueError() << errmsg{"CookedFrameSource requires a source_label"});
+    }
+    m_inputTag = cfg["art_tag"].asString();
+
+    m_tick = cfg["tick"].asDouble();
+    for (auto jtag : cfg["frame_tags"]) {
+        m_frame_tags.push_back(jtag.asString());
+    }
+    m_nticks = get(cfg, "nticks", m_nticks);
+}
+
+
+
+// is this the right way to diff an art::Timestamp?  
+static
+double tdiff(const art::Timestamp& ts1, const art::Timestamp& ts2)
+{
+    TTimeStamp tts1(ts1.timeHigh(), ts1.timeLow());
+    TTimeStamp tts2(ts2.timeHigh(), ts2.timeLow());
+    return tts2.AsDouble() - tts1.AsDouble();
+}
+
+
+static
+SimpleTrace* make_trace(const recob::Wire& rw, unsigned int nticks_want)
+{
+    // uint
+    const raw::ChannelID_t chid = rw.Channel();
+    const int tbin = 0;
+    const std::vector<float> sig = rw.Signal();
+
+    const float baseline = 0.0;
+    unsigned int nsamp = sig.size();
+    if (nticks_want > 0) {
+	nsamp = std::min(nsamp, nticks_want);
+    }
+
+    auto strace = new SimpleTrace(chid, tbin, nticks_want);
+    auto& q = strace->charge();
+    for (unsigned int itick=0; itick < nsamp; ++ itick) {
+	q[itick] = sig[itick];
+    }
+    for (unsigned int itick = nsamp; itick < nticks_want; ++itick) {
+	q[itick] = baseline;
+    }
+    return strace;
+}
+
+void CookedFrameSource::visit(art::Event & event)
+{
+    // fixme: want to avoid depending on DetectorPropertiesService for now.
+    const double tick = m_tick; 
+    art::Handle< std::vector<recob::Wire> > rwvh;
+    bool okay = event.getByLabel(m_inputTag, rwvh);
+    if (!okay) {
+        std::string msg = "CookedFrameSource failed to get raw::RawDigits: " + m_inputTag.encode();
+        std::cerr << msg << std::endl;
+        THROW(RuntimeError() << errmsg{msg});
+    }
+    else if (rwvh->size() == 0) return;
+
+    const std::vector<recob::Wire>& rwv(*rwvh);
+    const size_t nchannels = rwv.size();
+    std::cerr << "CookedFrameSource: got " << nchannels << " raw::RawDigit objects\n";
+
+    WireCell::ITrace::vector traces(nchannels);
+    for (size_t ind=0; ind<nchannels; ++ind) {
+        auto const& rw = rwv.at(ind);
+        traces[ind] = ITrace::pointer(make_trace(rw, m_nticks));
+	if (!ind) {             // first time through
+	    std::cerr << "\tnticks=" << rw.NSignal() << " setting to " << m_nticks << std::endl;
+	}
+    }
+
+    const double time = tdiff(event.getRun().beginTime(), event.time());
+    auto sframe = new WireCell::SimpleFrame(event.event(), time, traces, tick);
+    for (auto tag : m_frame_tags) {
+        //std::cerr << "\ttagged: " << tag << std::endl;
+        sframe->tag_frame(tag);
+    }
+    m_frames.push_back(WireCell::IFrame::pointer(sframe));
+    m_frames.push_back(nullptr);
+}
+
+bool CookedFrameSource::operator()(WireCell::IFrame::pointer& frame)
+{
+    frame = nullptr;
+    if (m_frames.empty()) {
+        return false;
+    }
+    frame = m_frames.front();
+    m_frames.pop_front();
+    return true;
+}
+// Local Variables:
+// mode: c++
+// c-basic-offset: 4
+// End:
---
+++ larwirecell/Components/CookedFrameSource.h
@@ -0,0 +1,50 @@
+/** A WCT component which is a source of "cooked" frames which it
+ * produces by also being an art::Event visitor.
+ *
+ * Cooked means that the waveforms are taken from the art::Event as a
+ * labeled std::vector<recob::Wire> collection.
+ */
+
+#ifndef LARWIRECELL_COMPONENTS_COOKEDFRAMESOURCE
+#define LARWIRECELL_COMPONENTS_COOKEDFRAMESOURCE
+
+#include "larwirecell/Interfaces/IArtEventVisitor.h"
+#include "WireCellIface/IFrameSource.h"
+#include "WireCellIface/IConfigurable.h"
+
+#include "canvas/Utilities/InputTag.h"
+
+#include <string>
+#include <vector>
+#include <deque>
+
+namespace wcls {
+    class CookedFrameSource : public IArtEventVisitor, 
+                              public WireCell::IFrameSource,
+                              public WireCell::IConfigurable { 
+    public:
+        CookedFrameSource();
+        virtual ~CookedFrameSource();
+
+        /// IArtEventVisitor 
+        virtual void visit(art::Event & event);
+
+        /// IFrameSource
+        virtual bool operator()(WireCell::IFrame::pointer& frame);            
+
+        /// IConfigurable
+        virtual WireCell::Configuration default_configuration() const;
+        virtual void configure(const WireCell::Configuration& config);
+
+    private:
+        std::deque<WireCell::IFrame::pointer> m_frames;
+        art::InputTag m_inputTag;
+        double m_tick;
+	int m_nticks;
+	std::vector<std::string> m_frame_tags;
+
+    };
+
+}
+
+#endif
